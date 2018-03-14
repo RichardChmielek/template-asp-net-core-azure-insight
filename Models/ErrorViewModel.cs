1e34376fbf294b822c6cb8df599a8f12fd2a6104
Richard Chmielek <github@chmielek.com>
2018-03-14 21:24:30 +0000

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace template_asp_net_core_azure_insight.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
